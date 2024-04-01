465f42d1a5e21a1f48b1f1700b32c209947387ae
Christopher Bryan <swimmer.chris@gmail.com>
2024-04-01 18:57:05 -0400

    updated for q4

--- q4.js
+++ q4.js
@@ -1,3 +1,22 @@
-function deckSort() {
+function deckSort(deck) {
+    const output = []
 
+    deck.sort((a, b) => {
+        return a - b;
+    });
+    console.log("sorted deck", deck)
+    while (deck.length > 0) {
+
+        const removeTop = deck.shift()
+        output.push(removeTop);
+        console.log("deck after removal", deck)
+        if (deck.length > 0) {
+            deck.push(deck.shift())
+            console.log("check after move to back", deck)
+        }
+    }
+    return output;
 }
+
+const deck = [17, 13, 11, 2, 3, 5, 7];
+console.log(deckSort(deck))
