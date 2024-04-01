3f7b459c0279dcabcb106a70d2dcdc385eaf6e15
Christopher Bryan <swimmer.chris@gmail.com>
2024-04-01 18:02:44 -0400

    updated q3

--- q3.js
+++ q3.js
@@ -1,5 +1,27 @@
 function heightChecker(heights) {
     const cloneHeights = [...heights]
+    count = 0;
+    let swapCount = 1;
 
+    while (swapCount > 0) {
+        swapCount = 0;
+        for (let i = 0; i < heights.length; i++) {
+            if (heights[i] > heights[i + 1]) {
+                const temp = heights[i + 1];
+                heights[i + 1] = heights[i];
+                heights[i] = temp;
+                swapCount++;
+            }
+        }
+    }
 
+    for (let i = 0; i < heights.length; i++) {
+        if (heights[i] !== cloneHeights[i]) {
+            count++
+        }
+    }
+    return count;
 }
+
+heights = [1, 1, 4, 2, 1, 3];
+console.log(heightChecker(heights))
